c2e5da09d0858032226e349b4b0b8a952877b41b
Austin <austinhardaway@gmail.com>
2018-11-28 17:42:03 -0500

    initial setup of Django/wsgi/mysql

---
+++ start.sh
@@ -0,0 +1,2 @@
+cowsay started project @ vm270/barcrawlapp
+screen uwsgi --socket 127.0.0.1:4305 --venv /home/student/projects/csci4300/csci4300-final-project --wsgi-file /home/student/projects/csci4300/csci4300-final-project/barcrawlapp/wsgi.py --master
